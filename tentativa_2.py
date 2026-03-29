faa45538f0d2b4afc6fb29521cb26819b1623f9a
Henrique Dos Santos Miranda” <fegemeos4@gmail.com>
2026-03-29 15:16:04 -0300

    tentativa fracassada

---
+++ tentativa_2.py
@@ -0,0 +1,8 @@
+numero_inteiro = input("Em qual numero você está pensando? ")
+numero_quebrado = input("Novamente, em qual numero você está pensando? ")
+
+type(numero_inteiro)
+type(numero_quebrado)
+
+print(str(f"O seu primeiro numero é {type(numero_inteiro)} e o segundo numero é {type(numero_quebrado)}."))
+# não entendi como que se usa o type, então na aula do professor eu aprendo
