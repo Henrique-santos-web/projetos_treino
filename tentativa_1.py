bbe71072a3ed6e4974c71173a046a4d4ac7e4bbf
Henrique Dos Santos Miranda” <fegemeos4@gmail.com>
2026-03-29 12:41:59 -0300

    Treino simples realizado com sucesso

---
+++ tentativa_1.py
@@ -0,0 +1,6 @@
+print("Atualmente o dólar está custando R$5,27 reais.")
+
+real = float(input("Quantos reais você deseja converter em dólares? "))
+dolar = real / 5.27
+
+print(f"Com R${real:.2f} você você tem US${dolar:.2f}.")
